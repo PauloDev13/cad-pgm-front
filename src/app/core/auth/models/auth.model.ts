848ef648a77d273d98a81fd456759597ae4f8522
Paulo Roberto De Morais <prmorais1302@gmail.com>
2026-04-15 16:25:31 -0300

    Starting the implementation of security with JWT tokens.

--- src/app/core/auth/models/auth.model.ts
+++ src/app/core/auth/models/auth.model.ts
@@ -24,3 +24,23 @@ export interface ResetPasswordDialogData {
   password: string;
 }
 
+// 1. O que vem do Backend no momento do login
+export interface IAuthResponse {
+  token: string; // O famoso JWT
+  forcePasswordChange: boolean; // Presumi que o backend ainda devolve isso aqui
+}
+
+// 2. O que nós vamos ler "escondido" dentro do Token (Payload do JWT)
+export interface IDecodedToken {
+  sub: string; // É aqui que o backend costuma guardar o userName/login
+  roles: string[]; // As permissões ("ROLE_ADMIN", "ROLE_USER")
+  exp: number; // Data de expiração
+}
+
+// 3. O estado do usuário que ficará disponível no Signal para o nosso Frontend inteiro
+export interface ILoggedUser {
+  userName: string;
+  roles: string[];
+  token: string;
+  forcePasswordChange: boolean;
+}
